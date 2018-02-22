a0d1f769ab56ca52d937caf8e35a2076910a1897
Patrick Vasile <p.vasile45@gmail.com>
2018-02-21 23:41:22 -0500

    bang is working

---
+++ Assets/BangScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BangScript : MonoBehaviour {
+	[SerializeField] Image bangImage;
+
+	void Start() {
+
+		bangImage.enabled = false;
+	}
+
+
+	void Update() {
+		if (Input.GetKeyDown ("space")) {
+
+			bangImage.enabled = true;
+
+		} if (Input.GetKeyUp ("space")) {
+
+			bangImage.enabled = false;
+		}
+	}
+}
---
+++ Assets/OwScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+	public class OwScript : MonoBehaviour
+	{
+		[SerializeField] Image OwImage;
+
+	void Start () 
+	{
+		OwImage.enabled = false;
+
+	}
+
+	void OnTriggerEnter (Collider other)
+		{
+		if(other.tag == "Player")
+		{ 
+			OwImage.enabled = true;
+		}
+			else { 
+			OwImage.enabled = false;
+	}
+}
+}
